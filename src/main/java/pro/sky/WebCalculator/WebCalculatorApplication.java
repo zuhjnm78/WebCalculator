31cc207731f9fffdfa457bc9e933b967ebcab5b6
oleghenkel <oleg.henkel75@gmail.com>
2023-06-06 22:37:40 +0200

    HomeWork WebCalculator

---
+++ src/main/java/pro/sky/WebCalculator/WebCalculatorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.WebCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/WebCalculator/WebCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.WebCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
